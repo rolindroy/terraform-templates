b60d714d9f6b593a431af866d6c1680f8ccf751c
Rolind Roy <rolind.roy@gmail.com>
2018-03-07 16:05:50 +0530

    updated aws-terraform templates

---
+++ aws-templates/aws.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+## AWS KeyPair
+resource "aws_key_pair" "add_terraform_key" {
+  key_name   = "terraform-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDllVejIyQpF31DzmGRhJEZcAaDRv82i7E/BkamG/nAVekxug81VYUgKstC0iEzhGGid2Mrtk0WFso8V146L4tZR3+20mKhaa4Ng/DNCT3BmEnTYhk6Bq+p8Z1v11dRJCOPbBQ15VNzMcBphbkxePXITsYcvZQ3AB0PCzTH9xtrkkFK06IueFDaUCBs8Yp5gv9xPR7Me2/+QLTCkziwc/JoB91vAViGbLHQ9yEzPzOw2n2tzJkQYLgCnLX919X6+OSp31v97V6mmh/8oyb1woNa1e5UXEDc8yTNsrBaILsBasUFSF5nhKJUmdvffrcSgs5c9AkeiWEwUz/o3bhKSL4pr9OH0fvBXf1g1bL6wmfO9gsVxxX9Wvuy7pjWPUncjZhoeMN/BvEXDQFWPF3x7fIgM3Fl9RfOmYVMxsO52WmiKb7hz3EQLbzRffyfIaA9iXRnq9GH0ccGEKFJp+FAjmujSOzj9kxLgnNH8JSi4o7/931aUZ6K0qA4vqsvk8Uw5pmkPyWdllpm9PZ/M379DjOzWXiePVsRudDqBN9S/PrqJR3cR9ACNPs+SJngxsU7Fqpvr2Efh+2wZqvACZUifH0vt6NrPzPBkKTvOZJcUa/ZqQTG0y1ClVQSvjEUmmg++AbAR08ZtVZnXzfEMNsdbc//TrSTT7CSWSuKZMjQCwLKzw== hello@rolindroy"
+}
+
+## AWS VPC
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block           = "${var.vpc_network}.0.0/16"
+  instance_tenancy     = "dedicated"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "terraform"
+  }
+}
+
+## AWS Subnet_1
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = "${aws_vpc.terraform_vpc.id}"
+  cidr_block        = "${var.vpc_network}.1.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags {
+    Name = "subnet-1"
+  }
+}
+
+## AWS Subnet_2
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = "${aws_vpc.terraform_vpc.id}"
+  cidr_block        = "${var.vpc_network}.2.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags {
+    Name = "subnet-2"
+  }
+}
+
+## AWS Internet gateway
+resource "aws_internet_gateway" "terrafom_gw" {
+  vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+  tags {
+    Name = "terraform_gw"
+  }
+}
+
+## AWS Route Table
+resource "aws_route_table" "terraform_public_route" {
+	vpc_id = "${aws_vpc.terraform_vpc.id}"
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.terrafom_gw.id}"
+	}
+}
+
+## AWS Public Subnet
+resource "aws_route_table_association" "terraform_public_subnet_1" {
+	subnet_id = "${aws_subnet.subnet_1.id}"
+	route_table_id = "${aws_route_table.terraform_public_route.id}"
+}
---
+++ aws-templates/variable.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  default = "AKIAIE476S7DQKGTQSZA"
+}
+
+variable "aws_secret_key" {
+  default = "Z+nO9zJosK5oKGVchboN5k7I4Pgg7qWmQAKye4Xo"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_network" {
+  default = "10.0"
+}
